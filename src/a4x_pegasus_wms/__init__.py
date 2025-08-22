67d9566734d6977cfe85bb78dc3d9962f15b258f
Rajiv Mayani <mayani@isi.edu>
2025-08-22 13:37:23 -0700

    chore: incorporate changes as per changes in a4x-orchestration

--- src/a4x_pegasus_wms/__init__.py
+++ src/a4x_pegasus_wms/__init__.py
@@ -6,11 +6,15 @@ _extended_summary_
 from __future__ import annotations
 
 import logging
+import os
 from functools import wraps
 from typing import TYPE_CHECKING, Callable, TypeVar
 
+from a4x.orchestration import Path as A4XPath
 from Pegasus.api import (
+    # Arch,
     Job,
+    Properties,
     ReplicaCatalog,
     Transformation,
     TransformationCatalog,
@@ -62,6 +66,10 @@ class PegasusWMS:
         self._a4x_workflow: A4XWorkflow = workflow
         self._pegasus_workflow: Workflow | None = None
         self._log = logging.getLogger(__name__)
+        self._props = Properties()
+        self._props["pegasus.mode"] = "development"
+        if "JAVA_HOME" in os.environ:
+            self._props["env.JAVA_HOME"] = os.environ["JAVA_HOME"]
 
     @check
     def plan(self) -> None:
@@ -69,6 +77,7 @@ class PegasusWMS:
 
         _extended_summary_
         """
+        self._props.write()
         self._pegasus_workflow.plan()  # type: ignore[union-attr]
 
     @check
@@ -128,7 +137,8 @@ class PegasusWMS:
         wf = self._pegasus_workflow = Workflow(name=a4wf.name)
         tfs = set()
 
-        for task in a4wf.tasks:
+        for _task in a4wf.graph:
+            task = a4wf.graph.nodes[_task]["task"]
             self._log.debug(f"Adding task {task.task_name} to Pegasus workflow")
             job = self._transform_task(task)
             tf = Transformation(
@@ -136,9 +146,10 @@ class PegasusWMS:
                 site="local",
                 pfn=task.exe_path.resolve(),
                 is_stageable=True,
+                # arch=Arch.AARCH64,
             )
             tfs.add(tf)
-            wf.add_job(job)
+            wf.add_jobs(job)
 
         self._log.debug(f"Adding {len(tfs)} transformations to Pegasus workflow")
         tc = TransformationCatalog()
@@ -149,23 +160,46 @@ class PegasusWMS:
         self._log.debug("Adding replicas to Pegasus workflow")
         rc = ReplicaCatalog()
         wf.add_replica_catalog(rc)
-        for wf_input in a4wf.task_inputs_from_graph():
-            wf.add_replicas("local", wf_input, str(wf_input.resolve()))
+        for _, wf_inputs in a4wf.task_inputs_from_graph.items():
+            if wf_inputs:
+                for wf_input in wf_inputs:
+                    self._log.debug(
+                        f"Adding replica {wf_input.path.name} to Pegasus workflow"
+                    )
+                    if wf_input.is_logical:
+                        self._log.debug(f"Skipping logical input {wf_input.path.name}")
+                        continue
+
+                    rc.add_replica("local", wf_input.path.name, wf_input.path.resolve())
 
     def _transform_task(self, task: Task) -> Job:
         job = Job(task.task_name)
-        if task.args:
-            job.add_args(task.args)
+
+        for arg in task.args or []:
+            if isinstance(arg, (A4XPath, os.PathLike)):
+                arg = get_path(arg)
+            job.add_args(arg)
+
         if task.inputs:
-            job.add_inputs(task.inputs, task.add_input_extra_kwargs)
+            job.add_inputs(
+                *[get_path(input) for input in task.inputs],
+                **task.add_input_extra_kwargs,
+            )
+
         if task.outputs:
-            job.add_outputs(task.outputs, task.add_output_extra_kwargs)
+            job.add_outputs(
+                *[get_path(output) for output in task.outputs],
+                **task.add_output_extra_kwargs,
+            )
+
         if task.stdin:
-            job.add_stdin(task.stdin)
+            job.add_stdin(get_path(task.stdin))
+
         if task.stdout:
-            job.add_stdout(task.stdout)
+            job.add_stdout(get_path(task.stdout))
+
         if task.stderr:
-            job.add_stderr(task.stderr)
+            job.add_stderr(get_path(task.stderr))
 
         if task.environment:
             for name, value in task.environment.items():
@@ -184,10 +218,13 @@ class PegasusWMS:
             job.add_pegasus_profile(cores=resources["num_cores"])
 
         if "num_nodes" in resources:
-            job.add_pegasus_profile(cores=resources["num_nodes"])
+            job.add_pegasus_profile(nodes=resources["num_nodes"])
 
         if "num_task" in resources:
-            job.add_pegasus_profile(cores=resources["num_tasks"])
+            job.add_pegasus_profile(cores=resources["num_task"])
+
+        if "gpus_per_node" in resources:
+            job.add_pegasus_profile(gpus=resources["gpus_per_node"])
 
         if "per_resource_type" in resources:
             job.add_pegasus_profile(cores=resources["per_resource_type"])
@@ -195,8 +232,16 @@ class PegasusWMS:
         if "per_resource_task_count" in resources:
             job.add_pegasus_profile(cores=resources["per_resource_task_count"])
 
-        if "gpus_per_node" in resources:
-            job.add_pegasus_profile(cores=resources["gpus_per_node"])
-
         if "exclusive" in resources:
             job.add_pegasus_profile(cores=resources["exclusive"])
+
+
+def get_path(path: A4XPath | os.PathLike | str) -> str:
+    """Transform a path to a string for use in Pegasus workflow objects."""
+    if isinstance(path, A4XPath):
+        return str(path.path) if path.is_logical else path.path.name
+
+    if isinstance(path, os.PathLike):
+        return str(path)
+
+    return path
